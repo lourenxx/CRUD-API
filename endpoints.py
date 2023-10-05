e15f0e7a02851641fcd53c6ee3ed135a3768f406
lourenxx <guilhermepxl7@gmail.com>
2023-10-05 16:28:50 -0300

    add: POST route

--- endpoints.py
+++ endpoints.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from models import DiscordMembers
 from app import app
+from db import db
 
 #Endpoint da aplicação
 
@@ -14,9 +15,31 @@ def get_member():
     return jsonify(member_get_json)
 
 
-@app.route("members/<id>")
+# GET BY ID
+
+@app.route("/members/<id>", methods=["GET"])
 def get_member_id(id):
     member_id = DiscordMembers.query.filter_by(id=id).first()
     member_id_json = [id.json() for id in member_id]
 
     return jsonify(member_id_json)
+
+# POST
+
+@app.route("/create", methods=["POST"])
+def post_member():
+    body = request.get_json()
+
+# VALIDATING THE PARAMETERS
+
+    try:
+        member = DiscordMembers(name=body["name"], nickname=body["nickname"], age=body["age"])
+        db.session.add(member)
+        db.session.commit()
+        return jsonify(201, "member", member.to_json(), "Succesfully created!")
+    
+    except Exception as e:
+        print(e)
+        return jsonify(400, "member", {}, "Register error!")
+
+    
