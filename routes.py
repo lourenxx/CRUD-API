b17f3724829a55196a5568882795ab7190ca9972
lourenxx <guilhermepxl7@gmail.com>
2024-01-31 18:56:17 -0300

    finished all the routes

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request, render_template
+from flask import Blueprint, jsonify, request, render_template, flash, redirect, url_for
 from models import DiscordMembers
 from db import db
 
@@ -6,75 +6,71 @@ from db import db
 members_blueprint = Blueprint('members', __name__)  
 
 # Rota para listar todos os membros
-@members_blueprint.route('/members', methods=['GET'])
+@members_blueprint.route('/', methods=['GET'])
 def get_member():
     members = DiscordMembers.query.all()
-    return render_template("members.html", members=members)
-    
-    
-
-# Rota para listar membros por id 
-@members_blueprint.route('/members/<id>', methods=['GET'])
-def get_member_by_id(id):
-    try:
-        member_id = DiscordMembers.query.filter_by(id=id).first()
-        member_id_list = member_id.json()
-        return jsonify({'member': member_id_list}), 200
-    except Exception as e:
-        return jsonify({'message': 'Erro ao buscar membro por id'}), 500
+    return render_template("index.html", members=members)
     
 
-# Rota pra criar os membros  
-@members_blueprint.route('members', methods=["POST"])
+# Rota pra criar os membros
+@members_blueprint.route('/create')
+def render_form():
+     return render_template("new.html") 
+
+
+@members_blueprint.route('/create', methods=["POST"])
 def post_member():
-    try:
-        data = request.get_json()
-        new_member = DiscordMembers(name=data['name'], nickname=data['nickname'], age=data['age'])
-        db.session.add(new_member)
-        db.session.commit()
-        return jsonify({'message': 'Membro criado com sucesso', 'member': new_member.json()}), 201
-    except Exception as e:
-        return jsonify({'message': 'Erro ao criar membro'}), 500
+    name = request.form['name']
+    nickname = request.form['nickname']
+    age = request.form['age']
+
+    member = DiscordMembers.query.filter_by(nickname=nickname).first()
+
+    if member:
+         flash("Membro ja existente!")
+         return redirect(url_for('members.get_member'))
     
+    new_member = DiscordMembers(name=name, nickname=nickname, age=age)
+    db.session.add(new_member)
+    db.session.commit()
+
+    flash("Membro adicionado com sucesso!")
+    return redirect(url_for('members.get_member'))
+
+
 
 # Rota para atualizar membros
-@members_blueprint.route('/update/<int:id>', methods=['PUT'])
-def uptade_member(id):
-    try:
-        data = request.get_json()
-        member = DiscordMembers.query.get(id)
+@members_blueprint.route('/update/<int:id>')
+def render_update(id):
+    member = DiscordMembers.query.filter_by(id=id).first()
+    return render_template("update.html", member=member)
 
-        if member:
-            member.name = data.get('name', member.name)
-            member.nickname = data.get('nickname', member.nickname)
-            member.age = data.get('age', member.age)
 
-            db.session.commit()
+@members_blueprint.route('/update/<int:id>', methods=['PUT', 'POST'])
 
-            return jsonify({'message': 'Membro atualizado com sucesso', 'member': member.json()}), 200
-        else:
-                return jsonify({'message': 'Membro não encontrado'}), 404
-        
-    except Exception as e:
-            return jsonify({'message': 'Erro ao atualizar membro'}), 500
-            
+def update_member(id):
+
+    member = DiscordMembers.query.get(id)
+    member.name = request.form.get('name', member.name)
+    member.nickname = request.form.get('nickname', member.nickname)
+    member.age = request.form.get('age', member.age)
+
+    db.session.add(member)
+    db.session.commit()
+
+    flash("Membro atualizado com sucesso!")
+    return redirect(url_for('members.get_member'))
+
 
 # Rota para deletar membros
-@members_blueprint.route('/delete/<int:id>', methods=['DELETE'])
+@members_blueprint.route('/delete/<int:id>', methods=['DELETE', 'POST'])
 def delete_member(id):
      
-     try:
-        member = DiscordMembers.query.get(id)
+    DiscordMembers.query.filter_by(id=id).delete()
+    db.session.commit()
 
-        if member:
-            db.session.delete(member)
-            db.session.commit()
-            return jsonify({'message' : f' Membro excluido {member} com sucesso'}), 204
-        else:
-            return jsonify({'message' : f' Membro não encontrado'}), 404
-        
-     except Exception as e:
-         return jsonify({'message' : 'Erro ao excluir membro'}), 500
+    flash("Membro deletado com sucesso!")
+    return redirect(url_for('members.get_member'))
 
 
             
